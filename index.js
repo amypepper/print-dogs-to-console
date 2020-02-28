286fa96db436e046a5bfdbe322a308fb661d6a02
Amy Pepper <amyandmicahpepper@gmail.com>
2020-02-28 12:16:20 -0800

    final commit

--- index.js
+++ index.js
@@ -8,13 +8,24 @@ function handleUserInput() {
     const userInput = $('input[id="dog-pics"]').val();
     // add that number to the dogAPI url
     const dogApi = `https://dog.ceo/api/breeds/image/random/${userInput}`;
-    console.log(dogApi);
-    //use fetch to call the API
-    fetch(dogApi)
-      .then(response => response.json())
-      .then(dogPics => console.log("these are the pics: ", dogPics))
-      .catch(err => console.log("error: ", err));
+
+    // prevent API from being called if user enters number over 50
+    if (userInput > 50) {
+      $(".image-results").html(
+        `<p role="alert" class="error-msg">That's too many dogs! Please choose a number between 1 and 50</p>`
+      );
+    } else {
+      callDogApi(dogApi);
+    }
   });
+}
+
+function callDogApi(url) {
+  //use fetch to call the API
+  fetch(url)
+    .then(response => response.json())
+    .then(dogPics => console.log("these are the pics: ", dogPics))
+    .catch(err => console.log("error: ", err));
 }
 
 handleUserInput();
